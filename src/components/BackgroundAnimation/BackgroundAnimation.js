3886fdf324fd0761c5c4eea315f74ecc885b8660
Carlos Aldrim <carlosaldrimfilho@gmail.com>
2025-03-16 22:11:53 -0300

    feature: Alterando fundo animado

--- src/components/BackgroundAnimation/BackgroundAnimation.js
+++ src/components/BackgroundAnimation/BackgroundAnimation.js
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./BackgroundAnimation.module.css";
 
 const BackgroundAnimation = () => {
-  return (
-    <div className={styles.backgroundShapes}>
-      <div className={styles.shape1}></div>
-      <div className={styles.shape2}></div>
-      <div className={styles.shape3}></div>
-      <div className={styles.shape4}></div>
-      <div className={styles.shape5}></div>
-      <div className={styles.shape6}></div>
-      <div className={styles.shape7}></div>
-      <div className={styles.shape8}></div>
-    </div>
-  );
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    let particlesArray = [];
+    const numParticles = 100;
+
+    class Particle {
+      constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 3 + 1;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+      }
+
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x <= 0 || this.x >= canvas.width) this.speedX *= -1;
+        if (this.y <= 0 || this.y >= canvas.height) this.speedY *= -1;
+      }
+
+      draw() {
+        ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+      }
+    }
+
+    function initParticles() {
+      particlesArray = [];
+      for (let i = 0; i < numParticles; i++) {
+        particlesArray.push(new Particle());
+      }
+    }
+
+    function animateParticles() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      particlesArray.forEach((particle) => {
+        particle.update();
+        particle.draw();
+      });
+      requestAnimationFrame(animateParticles);
+    }
+
+    initParticles();
+    animateParticles();
+
+    window.addEventListener("resize", () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      initParticles();
+    });
+  }, []);
+
+  return <canvas ref={canvasRef} className={styles.canvasBackground}></canvas>;
 };
 
 export default BackgroundAnimation;
